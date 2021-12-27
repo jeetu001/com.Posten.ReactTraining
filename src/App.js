5427a587b4d18fb9f73f5f5da4bb35b9c1592222
jeetu001 <ya.jitendra@tcs.com>
2021-12-27 13:17:20 +0530

    Logincommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from './Button';
+import Login from './Login';
+import {header,buttons,words} from './Heading.css'
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className='App'> 
+        <div className='header'>POSTEN WEBPAGE</div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+        <Login></Login>
+      </div>
+
   );
 }
 
